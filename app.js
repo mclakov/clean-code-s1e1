30a9a2f7775c9ee4da195082f4c8c70d84547e16
Roman Maklakov <mclakov@gmail.com>
2022-10-31 22:35:31 +0300

    fix: replace tag selectors for classes in app.js according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -33,17 +33,20 @@ var createNewTaskElement=function(taskString){
   var deleteButtonImg=document.createElement("img");//delete button image
 
   label.innerText=taskString;
-  label.className='task';
+  label.className='task-label task';
+
+  listItem.className='tasks-list-item';
 
   //Each elements, needs appending
   checkBox.type="checkbox";
+  checkBox.className='task-checkbox'
   editInput.type="text";
   editInput.className="task";
 
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className="edit";
+  editButton.className="button edit";
 
-  deleteButton.className="delete";
+  deleteButton.className="button delete";
   deleteButtonImg.src='./remove.svg';
   deleteButton.appendChild(deleteButtonImg);
 
@@ -82,8 +85,8 @@ var editTask=function(){
 
   var listItem=this.parentNode;
 
-  var editInput=listItem.querySelector('input[type=text]');
-  var label=listItem.querySelector("label");
+  var editInput=listItem.querySelector(".task-input");
+  var label=listItem.querySelector(".task-label");
   var editBtn=listItem.querySelector(".edit");
   var containsClass=listItem.classList.contains("edit-mode");
   //If class of the parent is .edit-mode
@@ -155,9 +158,9 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
   console.log("bind list item events");
 //select ListItems children
-  var checkBox=taskListItem.querySelector("input[type=checkbox]");
-  var editButton=taskListItem.querySelector("button.edit");
-  var deleteButton=taskListItem.querySelector("button.delete");
+  var checkBox=taskListItem.querySelector(".task-checkbox");
+  var editButton=taskListItem.querySelector(".button.edit");
+  var deleteButton=taskListItem.querySelector(".button.delete");
 
 
   //Bind editTask to edit button.
